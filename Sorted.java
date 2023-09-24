5b1f939d8969515ecf228e4492fd80684967658f
AbdullahAlumran <3boodmohd12@gmail.com>
2023-09-24 12:33:12 +0300

    Insertion Sort method

--- Sorted.java
+++ Sorted.java
@@ -5,8 +5,8 @@ public static void insertionSort(int[] arr) {
         int key = arr[i];
         int j = i - 1;
 
-        while (j >= 0 && arr[j] > key) {
-        arr[j + 1] = arr[j];
+        while (j >= 0 && arr[j] > key){
+        arr[j+1]=arr[j];
         j--;
         }
 
